3c01f0b25e0063bd65c45b7eed35d551902e7e1e
demri <johnsondemri@gmail.com>
2025-03-25 20:01:50 -0600

    made joke category joke buttons  and added category titles and started the joke retrieval function

---
+++ joke.js
@@ -0,0 +1,16 @@
+const categorytitles = {
+    'Any': 'Random Joke', 
+     'Programming':'Programing joke',
+    'Misc': "Miscellaneous Joke",
+     'Pun': 'Pun Joke',
+      'Spooky': 'Spooky Joke', 
+      'Christmas': 'Christmas Joke'
+};
+
+async function getJoke (category = 'Any') {
+    const setupELement = document.getElementById('setup');
+    const punchlineElement = document.getElementById('punchline');
+    const categorytitlesElement = document.getElementById('joke-category-title');
+    
+    
+};
